7b956d4c9d8689820f2a017307fae2850696c6cd
Sam Cain <samuelrcain@gmail.com>
2022-01-16 03:03:59 -0600

    Moved Color Def

--- Reality/Colors.h
+++ Reality/Colors.h
@@ -23,6 +23,37 @@ namespace colors {
 	const float Black[4]  = { 0.0f, 0.0f, 0.0f, 1.0f };
 	const float White[4]  = { 1.0f, 1.0f, 1.0f, 1.0f };
 
+	typedef struct Color {
+		float rgba[4];
+
+		Color& operator+=(const Vec3f& rhs)
+		{
+			rgba[0] += rhs.x;
+			rgba[1] += rhs.y;
+			rgba[2] += rhs.z;
+			return *this;
+		}
+
+		Color& operator-=(const Vec3f& rhs)
+		{
+			rgba[0] -= rhs.x;
+			rgba[1] -= rhs.y;
+			rgba[2] -= rhs.z;
+		}
+
+		Color(const float color[4])
+		{
+			rgba[0] = color[0];
+			rgba[1] = color[1];
+			rgba[2] = color[2];
+			rgba[3] = color[3];
+		}
+
+		operator float* () {
+			return rgba;
+		}
+
+	} Color;
 	/**
 	 * @brief 				Change color by amount in rotator
 	 * 						no bounds checking
--- Reality/Defs.h
+++ Reality/Defs.h
@@ -53,35 +53,3 @@ typedef struct Triangle2D
 	PositionVertex2D& vertB;
 	PositionVertex2D& vertC;
 } Triangle2D;
-
-typedef struct Color {
-	float rgba[4];
-
-	Color& operator+=(const Vec3f& rhs) 
-	{
-		rgba[0] += rhs.x;
-		rgba[1] += rhs.y;
-		rgba[2] += rhs.z;
-		return *this;
-	}
-
-	Color& operator-=(const Vec3f& rhs) 
-	{
-		rgba[0] -= rhs.x;
-		rgba[1] -= rhs.y;
-		rgba[2] -= rhs.z;
-	}
-
-	Color(const float color[4]) 
-	{
-		rgba[0] = color[0];
-		rgba[1] = color[1];
-		rgba[2] = color[2];
-		rgba[3] = color[3];
-	}
-
-	operator float* () {
-		return rgba;
-	}
-
-} Color;
